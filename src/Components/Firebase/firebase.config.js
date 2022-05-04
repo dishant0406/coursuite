d20448cd153d552d4513405f2677ba5355cf00e6
Dishant Sharma <dishu5570@gmail.com>
2022-05-05 00:28:29 +0530

    container improvment v3

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -2,23 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBVBsljz5mapve2dwRlG715ftY-gGDjHdk",
-  authDomain: "repomanagement-66868.firebaseapp.com",
-  projectId: "repomanagement-66868",
-  storageBucket: "repomanagement-66868.appspot.com",
-  messagingSenderId: "201762735158",
-  appId: "1:201762735158:web:40ce8e1523820209300497"
-};
-
 // const firebaseConfig = {
-//   apiKey: "AIzaSyDwHVJpApMgqxqjGGNkaTXcTmgteWv-VQc",
-//   authDomain: "yellomonkewebgl.firebaseapp.com",
-//   projectId: "yellomonkewebgl",
-//   storageBucket: "yellomonkewebgl.appspot.com",
-//   messagingSenderId: "508393349368",
-//   appId: "1:508393349368:web:6d62986bd9460fab5adb0d"
+//   apiKey: "AIzaSyBVBsljz5mapve2dwRlG715ftY-gGDjHdk",
+//   authDomain: "repomanagement-66868.firebaseapp.com",
+//   projectId: "repomanagement-66868",
+//   storageBucket: "repomanagement-66868.appspot.com",
+//   messagingSenderId: "201762735158",
+//   appId: "1:201762735158:web:40ce8e1523820209300497"
 // };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwHVJpApMgqxqjGGNkaTXcTmgteWv-VQc",
+  authDomain: "yellomonkewebgl.firebaseapp.com",
+  projectId: "yellomonkewebgl",
+  storageBucket: "yellomonkewebgl.appspot.com",
+  messagingSenderId: "508393349368",
+  appId: "1:508393349368:web:6d62986bd9460fab5adb0d"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/Components/RepoCard/RepoCard.jsx
+++ src/Components/RepoCard/RepoCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {
+  Menu,
+  MenuItem,
+  MenuButton
+} from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+import '@szhsin/react-menu/dist/transitions/slide.css';
+import Modal from 'react-awesome-modal';
+
+import {DotsVerticalRounded} from '@styled-icons/boxicons-regular'
 import './RepoCard.css'
 // import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { doc, deleteDoc } from "firebase/firestore";
 import {db} from '../Firebase/firebase.config'
 const RepoCard = ({data, id}) => {
+  const [modelstate, setModelState] = React.useState(false);
   let link = '';
   if(data.link.includes('https://')){
     link = data.link
@@ -17,7 +28,41 @@ const RepoCard = ({data, id}) => {
     
     window.location.reload(false);
   }
+
+  const handleBtnClick = async (e)=>{
+    await handleDeleteClick(e)
+    await closeModal()
+
+  }
+
+ function openModal() {
+  setModelState(true);
+}
+
+function closeModal() {
+  setModelState(false)
+}
   return (
+    <div>
+    <div>
+              <Modal 
+                    visible={modelstate}
+                    width="250"
+                    height="200"
+                    effect="fadeInUp"
+                    onClickAway={() => closeModal()}
+                >
+                  <div className="model-cont">
+                <div className='model-div-cont'>
+                  <h3 className='model-h3'>{`Are you sure you want to delete "${data.title.toUpperCase()}" !`}</h3>
+                  <div className="model-btn">
+                  <button className='button-37' onClick={handleBtnClick}>Yes</button>
+                  <button className='btn-no button-37' onClick={closeModal}>No</button>
+                  </div>
+                </div>
+                </div>
+                </Modal>
+    </div>
       <div className="repocard">
         <div className="repocard-header">
           <img src={data.imageUrl} alt="rover" />
@@ -33,9 +78,15 @@ const RepoCard = ({data, id}) => {
           </h4>
           <div className="btn-goto-a">
           <a href={link} target="_blank"><button className="button-24">Go to Project</button></a>
-          <button className="button-24" onClick={handleDeleteClick}>Delete</button>
+          {/* <button className="button-24" onClick={handleDeleteClick}>Delete</button> */}
+          <div>
+          <Menu menuButton={<DotsVerticalRounded style={{color: 'black', width: '35px'}}/>} transition>
+            <MenuItem onClick={openModal}>Delete Project</MenuItem>
+        </Menu>
+        </div>
           </div>
         </div>
+      </div>
       </div>
 
   );
